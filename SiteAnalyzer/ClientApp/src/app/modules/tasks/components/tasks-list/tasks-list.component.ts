e9acc2620f1b00bfb7f5a175f43b182e5a7225e3
Катя Недавняя <katrinka.nedavnyaya@gmail.com>
2018-10-23 00:40:34 +0300

    some changes

---
+++ SiteAnalyzer/ClientApp/src/app/modules/tasks/components/tasks-list/tasks-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core"
+
+@Component({
+  selector: "tasks-list",
+  templateUrl: "tasks-list.component",
+  styleUrls: ["tasks-list.component"]
+})
+export class TasksListComponent {
+
+}
